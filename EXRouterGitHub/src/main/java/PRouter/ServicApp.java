da606386f42d120e808971159054c7b9a39e8bc1
Adil M Ladadwah <Adil M Ladadwah@DESKTOP-BNO6O73>
2018-07-02 14:53:01 +0300

    77

--- EXRouterGitHub/src/main/java/PRouter/ServicApp.java
+++ EXRouterGitHub/src/main/java/PRouter/ServicApp.java
@@ -6,44 +6,40 @@ import java.net.SocketException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class ServicApp {
 
 	@RequestMapping("/")
 	public String index() {
-		
-		String Message="";
-		Message= ServiceMessage();
+
+		String Message = "";
+		Message = ServiceMessage();
 		return Message;
 	}
-	
 
 	@RequestMapping("/Version")
 	public String Version() throws SocketException, IOException {
-	
-	RouterAPIs.getInstance().connect();
-	RouterAPIs.getInstance().sendCommand("sh Version");
-	RouterAPIs.getInstance().disconnect();
-		
-	return RouterAPIs.ResponseCommand;
+
+		RouterAPIs.getInstance().connect();
+		RouterAPIs.getInstance().sendCommand("sh Version");
+		RouterAPIs.getInstance().disconnect();
+
+		return RouterAPIs.ResponseCommand;
 	}
-	
-	
+
 	@RequestMapping("/Interfaces")
 	public String Interfaces() throws SocketException, IOException {
-	
-	RouterAPIs.getInstance().connect();
-	RouterAPIs.getInstance().sendCommand("sh Version");
-	RouterAPIs.getInstance().disconnect();
-		
-	return RouterAPIs.ResponseCommand;
+
+		RouterAPIs.getInstance().connect();
+		RouterAPIs.getInstance().sendCommand("sh Version");
+		RouterAPIs.getInstance().disconnect();
+
+		return RouterAPIs.ResponseCommand;
 	}
+
 	public String ServiceMessage() {
-	
+
 		return "Greetings from Spring Boot!!!";
 	}
 
 }
-
-		
