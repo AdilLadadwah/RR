8c038c92e4cec291f3732c68e08801fbb2566bdc
Adil M Ladadwah <Adil M Ladadwah@DESKTOP-BNO6O73>
2018-07-03 11:21:36 +0300

    101

--- EXRouterGitHub/src/main/java/PRouter/ServicApp.java
+++ EXRouterGitHub/src/main/java/PRouter/ServicApp.java
@@ -2,9 +2,19 @@ package PRouter;
 
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @RestController
 public class ServicApp {
@@ -17,6 +27,13 @@ public class ServicApp {
 		return Message;
 	}
 
+	/**
+	 * Show Version of Router
+	 * 
+	 * @return
+	 * @throws SocketException
+	 * @throws IOException
+	 */
 	@RequestMapping("/Version")
 	public String Version() throws SocketException, IOException {
 
@@ -26,14 +43,23 @@ public class ServicApp {
 
 		return RouterAPIs.ResponseCommand;
 	}
-
+	/**
+	 * Show Version of Interfaces and their IP
+	 * 
+	 * @return
+	 * @throws SocketException
+	 * @throws IOException
+	 */
 	@RequestMapping("/Interfaces")
-	public String[] Interfaces() throws SocketException, IOException {
+	public String Interfaces() throws SocketException, IOException {
 
+		
+		// Send command  for router
 		RouterAPIs.getInstance().connect();
 		RouterAPIs.getInstance().sendCommand("show ip interface brief");
 		RouterAPIs.getInstance().disconnect();
 
+		// Get the response and get from it Interfaces and IP
 		RouterAPIs.ResponseCommand = RouterAPIs.ResponseCommand.replaceAll("( )+", " ");
 		String[] RCommand1 = RouterAPIs.ResponseCommand.split("\n");
 
@@ -54,14 +80,15 @@ public class ServicApp {
 
 		String[] SInt_IP = Int_IP.split("\n");
 
-		System.out.print("\nInterface \t\t IP-Address");
-
 		for (int i = 0; i < SInt_IP.length; i++)
 			System.out.print("\n" + SInt_IP[i]);
 
+		// Split between interface and IP and Create Hash Map
 		String[] Interface = new String[SInt_IP.length];
 		String[] IP = new String[SInt_IP.length];
 		String[] INTER_IP = new String[2];
+
+		HashMap<String, String> hmap = new HashMap<String, String>();
 
 		for (int i = 0; i < SInt_IP.length; i++)
 
@@ -69,9 +96,18 @@ public class ServicApp {
 			INTER_IP = SInt_IP[i].split(" ", 2);
 			Interface[i] = INTER_IP[0];
 			IP[i] = INTER_IP[1];
+
+			hmap.put(Interface[i], IP[i]);
 		}
 
-		return SInt_IP;
+		// Convert Hash map for gsonObject and convert it to String
+		Map<String, String> map = new TreeMap<String, String>(hmap);
+		Gson gsonObject = new GsonBuilder().setPrettyPrinting().create();
+		String JSON = gsonObject.toJson(map);
+		
+		// JSONObject JSON = new JSONObject(map);
+
+		return JSON;
 	}
 
 	public String ServiceMessage() {
